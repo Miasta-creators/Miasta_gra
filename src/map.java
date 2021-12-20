e69b47673f716720da8a6fb51d2026a8b74f9186
ATcat-pl <antektyczka@gmail.com>
2021-12-20 17:28:57 +0100

    v1.3
    Added chunks, props: tree

--- src/map.java
+++ src/map.java
@@ -1,9 +1,19 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Random;
 import java.util.Scanner;
 
 public class map {
+
+    public static Random random = new Random();
+    private static final byte[] BUFFER = new byte[4096 * 1024];
+
+    private static void copy(InputStream input, OutputStream output) throws IOException {
+        int bytesRead;
+        while ((bytesRead = input.read(BUFFER)) != -1) {
+            output.write(BUFFER, 0, bytesRead);
+        }
+    }
+
     public static int[][] gameMAP = {
             {1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1016},
             {1001, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
@@ -23,30 +33,77 @@ public class map {
             {1015, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
             {1016, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
     };
-    public static void save(String path) throws FileNotFoundException {
-        PrintWriter zapis = new PrintWriter("saves\\"+path);
-        for(int i=1; i<17; i++){
-            for(int j=1; j<17; j++){
-                zapis.print(gameMAP[i][j]+",");
+    public static int chunk_y = 0;
+    public static int chunk_x = 0;
+    public static String path = String.valueOf(random.nextInt());
+    private static File zipFilePath;
+
+    public static void save() throws IOException {
+        File f1 = new File("saves\\" + path);
+        f1.mkdir();
+        File f = new File("saves\\" + path + "\\" + chunk_x + "," + chunk_y + ".citymap");
+
+        PrintWriter zapis = new PrintWriter("saves\\" + path + "\\" + chunk_x + "," + chunk_y + ".citymap");
+        for (int i = 1; i < 17; i++) {
+            for (int j = 1; j < 17; j++) {
+                zapis.print(gameMAP[i][j] + ",");
             }
             zapis.print(";");
         }
         zapis.close();
+
+
     }
-    public static void load(String path) throws FileNotFoundException {
-        try {
-            Scanner odczyt = new Scanner(new File("saves\\" + path));
-            String line = odczyt.nextLine();
-            String[] line_split = line.split(";");
-            for (int j = 0; j < line_split.length; j++) {
-                String[] id_number = line_split[j].split(",");
-                for (int x = 0; x < id_number.length; x++) {
-                    map.gameMAP[j + 1][x + 1] = Integer.parseInt(id_number[x]);
+    public static void load() {
+        if (path.equals("null")) {
+            System.out.println("Ustawiono chunk na 0,0");
+        } else if (path.equals("grass")) {
+            for (int j = 0; j < gameMAP.length - 1; j++) {
+                for (int x = 0; x < gameMAP[j].length - 1; x++) {
+                    map.gameMAP[j + 1][x + 1] = 0;
                 }
             }
+            path = String.valueOf(random.nextInt());
+        } else if (path.equals("dirt")) {
+            for (int j = 0; j < gameMAP.length - 1; j++) {
+                for (int x = 0; x < gameMAP[j].length - 1; x++) {
+                    map.gameMAP[j + 1][x + 1] = 1;
+                }
+            }
+            path = String.valueOf(random.nextInt());
+        } else if (path.equals("water")) {
+            for (int j = 0; j < gameMAP.length - 1; j++) {
+                for (int x = 0; x < gameMAP[j].length - 1; x++) {
+                    map.gameMAP[j + 1][x + 1] = 2;
+                }
+            }
+            path = String.valueOf(random.nextInt());
+        } else if (path.equals("concrete")) {
+            for (int j = 0; j < gameMAP.length - 1; j++) {
+                for (int x = 0; x < gameMAP[j].length - 1; x++) {
+                    map.gameMAP[j + 1][x + 1] = 3;
+                }
+            }
+            path = String.valueOf(random.nextInt());
+        } else {
+            try {
+                Scanner odczyt = new Scanner(new File("saves\\" + path + "\\" + chunk_x + "," + chunk_y + ".citymap"));
+                String line = odczyt.nextLine();
+                String[] line_split = line.split(";");
+                for (int j = 0; j < line_split.length; j++) {
+                    String[] id_number = line_split[j].split(",");
+                    for (int x = 0; x < id_number.length; x++) {
+                        map.gameMAP[j + 1][x + 1] = Integer.parseInt(id_number[x]);
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Mapa nie istnieje");
+                for (int j = 0; j < gameMAP.length - 1; j++) {
+                    for (int x = 0; x < gameMAP[j].length - 1; x++) {
+                        map.gameMAP[j + 1][x + 1] = 0;
+                    }
+                }
+            }
+            }
         }
-        catch(FileNotFoundException e){
-            System.out.println("Podana mapa nie istnieje");
-        }
-    }
 }
--- src/miasta.java
+++ src/miasta.java
@@ -1,22 +1,27 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.*;
-import java.util.HashMap;
-
-
 import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
 
 
 
 @SuppressWarnings("serial")
 public class miasta extends JPanel {
+    public static String version = "1.3";
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
         initialize();
@@ -25,16 +30,16 @@ public class miasta extends JPanel {
                 try {
                     createAndShowGui1();
                     window2.createAndShowGui2();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+                catch(IOException e){}
             }
         });
+        if(version_compare()) System.out.println("Dostępna jest nowsza wersja pobierz ją tutaj: https://page.atcat.repl.co/program_downloads/gra_miasta");
         while(true){
             int wiersz=0;
             int kolumna=0;
             int kod=1000;
-            System.out.println("d - dalej / s - zapis / e - wyjście / l - wczytaj");
+            System.out.println("d - dalej / s - zapis / e - wyjście / l - wczytaj / z - zmień chunk");
             String opcja = scanner.nextLine().toLowerCase(Locale.ROOT);
             if(opcja.equals("d")) {
                 try {
@@ -53,15 +58,41 @@ public class miasta extends JPanel {
                 }
             }
             else if(opcja.equals("s")){
-                System.out.print("Naswa mapy: ");
-                map.save(scanner.nextLine()+".citymap");
+                System.out.print("Nazwa mapy: ");
+                map.path = scanner.nextLine();
+                map.save();
             }
             else if(opcja.equals("e")){
                 System.exit(0);
             }
             else if(opcja.equals("l")){
-                System.out.print("Naswa mapy: ");
-                map.load(scanner.nextLine()+".citymap");
+                map.chunk_x = 0;
+                map.chunk_y = 0;
+                System.out.print("Nazwa mapy: ");
+                map.path = scanner.nextLine();
+                map.load();
+                reloadmap();
+            }
+            else if(opcja.equals("z")){
+                System.out.println("w - UP / s - DOWN / a - LEFT / d - RIGHT");
+                opcja = scanner.nextLine().toLowerCase(Locale.ROOT);
+                if(opcja.equals("w")){
+                    map.save();
+                    map.chunk_y++;
+                }
+                else if(opcja.equals("s")){
+                    map.save();
+                    map.chunk_y--;
+                }
+                else if(opcja.equals("a")){
+                    map.save();
+                    map.chunk_x--;
+                }
+                else if(opcja.equals("d")){
+                    map.save();
+                    map.chunk_x++;
+                }
+                map.load();
                 reloadmap();
             }
 
@@ -75,6 +106,29 @@ public class miasta extends JPanel {
             }
             catch(IllegalComponentStateException e){}
         }
+    }
+
+    public static boolean version_compare(){
+        try{
+            URL url = new URL("https://page.atcat.repl.co/program_downloads/gra_miasta/lastest_version");
+
+            URLConnection urlCon = url.openConnection();
+            BufferedReader read = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+            String ver = read.readLine();
+            read.close();
+
+            if(ver.equals(version)){
+                return false;
+            }
+            else {
+                return true;
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public static void reloadmap(){
@@ -98,6 +152,9 @@ public class miasta extends JPanel {
         textures.put(1,"dirt.png");
         textures.put(2,"water.png");
         textures.put(3,"concrete.png");
+
+        textures.put(100,"props\\tree.png");
+
         textures.put(41,"roads\\road1.png");
         textures.put(42,"roads\\road2.png");
         textures.put(51,"roads\\road_crossing_3_1.png");
@@ -105,6 +162,23 @@ public class miasta extends JPanel {
         textures.put(53,"roads\\road_crossing_3_3.png");
         textures.put(54,"roads\\road_crossing_3_4.png");
         textures.put(55,"roads\\road_crossing_4.png");
+        textures.put(61,"roads\\road_turn_1.png");
+        textures.put(62,"roads\\road_turn_2.png");
+        textures.put(63,"roads\\road_turn_3.png");
+        textures.put(64,"roads\\road_turn_4.png");
+
+        textures.put(71,"roads\\path1.png");
+        textures.put(72,"roads\\path2.png");
+        textures.put(81,"roads\\path_crossing_3_1.png");
+        textures.put(82,"roads\\path_crossing_3_2.png");
+        textures.put(83,"roads\\path_crossing_3_3.png");
+        textures.put(84,"roads\\path_crossing_3_4.png");
+        textures.put(85,"roads\\path_crossing_4.png");
+        textures.put(91,"roads\\path_turn_1.png");
+        textures.put(92,"roads\\path_turn_2.png");
+        textures.put(93,"roads\\path_turn_3.png");
+        textures.put(94,"roads\\path_turn_4.png");
+
         textures.put(600,"buildings\\block_top.png");
         textures.put(601,"buildings\\block_corner_1.png");
         textures.put(602,"buildings\\block_corner_2.png");
@@ -114,16 +188,12 @@ public class miasta extends JPanel {
         textures.put(612,"buildings\\block_side_2.png");
         textures.put(613,"buildings\\block_side_3.png");
         textures.put(614,"buildings\\block_side_4.png");
-        textures.put(621,"buildings\\block_slant_1.png");
-        textures.put(622,"buildings\\block_slant_2.png");
-        textures.put(623,"buildings\\block_slant_3.png");
-        textures.put(624,"buildings\\block_slant_4.png");
         textures.put(631,"buildings\\block_2side_1.png");
         textures.put(632,"buildings\\block_2side_2.png");
         textures.put(640,"buildings\\house1.png");
         textures.put(641,"buildings\\house2.png");
 
-        textures.put(1000,"text\\null.png");
+        textures.put(1000, "text\\null.png");
         textures.put(1001,"text\\1.png");
         textures.put(1002,"text\\2.png");
         textures.put(1003,"text\\3.png");
@@ -182,7 +252,8 @@ public class miasta extends JPanel {
             return ImageIO.read(new File("textures\\error.png"));
         }
         catch (IOException e) {
-            return null;
+            System.out.println("ERROR Trying again");
+            return error_texture();
         }
     }
 
